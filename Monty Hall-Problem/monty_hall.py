53377bc335a8e71939b56f43b7d59033ab5d0cc3
Yudhishtiran Vajeeston <81691774+Mbiushelix@users.noreply.github.com>
2022-03-10 22:54:56 +0100

    Update monty_hall.py
    Faster code

--- Monty Hall-Problem/monty_hall.py
+++ Monty Hall-Problem/monty_hall.py
@@ -1,29 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import random as random
-import numpy as np
 
-n = int(10e6)
-monty = []
-win_index = [random.randint(1, 3) for x in range(0, n)]
-first_choice = [random.randint(1, 3) for y in range(0, n)]
+n = int(10e5)
+ref = [1, 2, 3]
+first_c_wins , second_c_wins = 0 , 0
 
-# Monty's choices
-for k in zip(win_index, first_choice):
-    ref = [1, 2, 3]
-    c = list(set(ref) - set(k))
-    monty.append(c[random.randint(0, len(c) - 1)])
-
-# Second choice 
-sec_choice = []
-for x, y in zip(first_choice, monty):
-    sec_choice.append(6 - (x + y))
-
-# Wins 
-first_c_wins = [1 if x == 0 else 0 for x in
-                list(np.array(win_index) - np.array(first_choice))]
-second_c_wins = [1 if x == 0 else 0 for x in
-                 list(np.array(win_index) - np.array(sec_choice))]
-
-print(f"Chance for winning without changing: {sum(first_c_wins)*100 / n}%")
-print(f"Chance for winning with change: {sum(second_c_wins)*100 / n}%")
+for i in range(n):
+    win_index = int(random.random() * 3 + 1)
+    first_choice = int(random.random() * 3 + 1)
+    c = list(set(ref)-set([win_index,first_choice]))
+    monty_choice = c[int(random.random() * len(c))]
+    sec_choice = 6 - (monty_choice + first_choice)
+    
+    if win_index == first_choice:
+        first_c_wins += 1
+    elif win_index == sec_choice:
+        second_c_wins += 1
+        
+print(f"Chance for winning without changing: {first_c_wins*100 / n}%")
+print(f"Chance for winning with change: {second_c_wins*100 / n}%")
